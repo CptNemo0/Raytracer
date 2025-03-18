7d654dc9830d0c057e78fec1a638935ffe9ff411
Paweł Stus <pawel.j.stus@gmail.com>
2025-03-18 20:38:35 +0100

    fix

--- Raytracer/main.cpp
+++ Raytracer/main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char** argv)
 	math::vec4 a(1.0f, 2.0f, 3.0f, 4.0f);
 	math::vec4 b(5.0f, 6.0f, 7.0f, 8.0f);
 	auto c = a + b;
+	c += c;
 
 	for (std::uint32_t i = 0; i < c.size(); ++i)
 	{
