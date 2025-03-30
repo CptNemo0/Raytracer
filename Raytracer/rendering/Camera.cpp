88f3f1a58c345f5da17e99d162cc3f36d031acf2
Katarzyna Tyszka <katarzynatysz66@gmail.com>
2025-03-31 01:12:58 +0200

    Camera separation by type

--- Raytracer/rendering/Camera.cpp
+++ Raytracer/rendering/Camera.cpp
@@ -1,12 +1,5 @@
 #include "Camera.h"
 
-std::pair<const float, const float> rendering::Camera::GetDimensions() const
-{
-	const float H = 2.0f * tanf(fov_ * 0.5f) * scale_;
-	const float W = aspect_ratio_ * H;
-	return std::make_pair(W, H);
-}
-
 math::vec3 rendering::Camera::GetPixelPosition(const float x, const float y) const
 {
 	math::vec3 return_value = position_;
@@ -17,3 +10,29 @@ math::vec3 rendering::Camera::GetPixelPosition(const float x, const float y) con
 
 	return return_value;
 }
+
+std::pair<const float, const float> rendering::PerspectiveCamera::GetDimensions() const
+{
+	const float H = 2.0f * tanf(fov_ * 0.5f) * scale_;
+	const float W = aspect_ratio_ * H;
+	return std::make_pair(W, H);
+}
+
+intersections::Ray rendering::PerspectiveCamera::GetRay(const float x, const float y) const
+{
+	math::vec3 pixel_position = GetPixelPosition(x, y);
+	math::vec3 ray_dir = (pixel_position - position_);
+	return intersections::Ray(position_, ray_dir);
+}
+
+std::pair<const float, const float> rendering::OrthographicCamera::GetDimensions() const
+{
+	return { scale_ * aspect_ratio_, scale_ };
+}
+
+intersections::Ray rendering::OrthographicCamera::GetRay(const float x, const float y) const
+{
+	math::vec3 pixel_position = GetPixelPosition(x, y);
+	return intersections::Ray(pixel_position, forward_);
+}
+
--- Raytracer/rendering/Camera.h
+++ Raytracer/rendering/Camera.h
@@ -2,6 +2,7 @@
 #define RENDERING_CAMERA_H
 
 #include "raytracer_math.h"
+#include "../intersections/Ray.h"
 
 namespace rendering
 {
@@ -14,18 +15,46 @@ namespace rendering
 		math::vec3 right_      = math::vec3(1.0f, 0.0f, 0.0f);
 		math::vec3 look_point_ = math::vec3(0.0f, 0.0f, 1.0f);
 		float aspect_ratio_ = 16.0f / 9.0f;
-		float fov_ = math::pid4;
 		float scale_ = 2.0f;
 
 		Camera() = default;
 
-		Camera(const math::vec3& position, const math::vec3& forward, const math::vec3& up, const math::vec3& right, const math::vec3& look_point, float aspect_ratio, float fov, float scale)
-			: position_(position), forward_(forward), up_(up), right_(right), look_point_(look_point), aspect_ratio_(aspect_ratio), fov_(fov), scale_(scale)
+		Camera(const math::vec3& position, const math::vec3& forward, const math::vec3& up, const math::vec3& right, const math::vec3& look_point, float aspect_ratio, float scale)
+			: position_(position), forward_(forward), up_(up), right_(right), look_point_(look_point), aspect_ratio_(aspect_ratio), scale_(scale)
 		{
 		}
 
-		std::pair<const float, const float> GetDimensions() const;
+		virtual std::pair<const float, const float> GetDimensions() const = 0;
+		virtual intersections::Ray GetRay(const float x, const float y) const = 0;
 		math::vec3 GetPixelPosition(const float x, const float y) const;
+
+	};
+
+	class PerspectiveCamera : public Camera
+	{
+	public:
+		float fov_ = math::pid4;
+
+		PerspectiveCamera(const math::vec3& position, const math::vec3& forward, const math::vec3& up, const math::vec3& right, const math::vec3& look_point, float aspect_ratio, float fov, float scale)
+			: Camera(position, forward, up, right, look_point, aspect_ratio, scale), fov_(fov){ }
+
+		PerspectiveCamera() = default;
+
+		std::pair<const float, const float> GetDimensions() const override;
+		intersections::Ray GetRay(const float x, const float y) const override;
+	};
+
+	class OrthographicCamera : public Camera
+	{
+	public:
+		OrthographicCamera(const math::vec3& position, const math::vec3& forward, const math::vec3& up, const math::vec3& right, const math::vec3& look_point, float aspect_ratio, float scale)
+			: Camera(position, forward, up, right, look_point, aspect_ratio, scale) {
+		}
+
+		OrthographicCamera() = default;
+
+		std::pair<const float, const float> GetDimensions() const override;
+		intersections::Ray GetRay(const float x, const float y) const override;
 	};
 }
 
