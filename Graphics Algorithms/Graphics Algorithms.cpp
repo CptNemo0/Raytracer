3239b60b8b58391275a3080de808831fcb0f7802
Katarzyna Tyszka <katarzynatysz66@gmail.com>
2025-04-10 13:53:39 +0200

    l4 finish

--- Graphics Algorithms/Graphics Algorithms.cpp
+++ Graphics Algorithms/Graphics Algorithms.cpp
@@ -9,21 +9,35 @@ int main()
 	Color4 color(0, 255, 0, 255);
 	colorBuffer.fillColorBuffer(color);
 
-	Color4 colorT(0, 0, 255, 255);
+	Color4 colorT(255, 0, 255, 255);
 
-	math::vec2 v0_ndc(0.5f, 0.9f);
-	math::vec2 v1_ndc(0.5f, 0.0f);
-	math::vec2 v2_ndc(0.0f, 0.0f);
+
+	math::vec3 v0_ndc(0.0f, -0.5f, 0.0f);
+	math::vec3 v1_ndc(-0.5f, 0.0f, 0.0f);
+	math::vec3 v2_ndc(0.0f, 0.8f, 3.0f);
+
+	math::vec3 v0(0.5f, 0.5f, 2.0f);
+	math::vec3 v1(-0.4f, -0.5f, 2.0f);
+	math::vec3 v2(0.0f, 1.0f, 2.0f);
 
     int height = colorBuffer.height_;
     int width = colorBuffer.width_;
 
-    math::vec2 v0_screen = { float((v0_ndc[0] + 1) * 0.5 * width), float((1 - v0_ndc[1]) * 0.5 * height) };
-    math::vec2 v1_screen = { float((v1_ndc[0] + 1) * 0.5 * width), float((1 - v1_ndc[1]) * 0.5 * height) };
-    math::vec2 v2_screen = { float((v2_ndc[0] + 1) * 0.5 * width), float((1 - v2_ndc[1]) * 0.5 * height) };
+    math::vec3 v0_screen = { float((v0_ndc[0] + 1) * 0.5 * width), float((1 - v0_ndc[1]) * 0.5 * height), v0_ndc[2] };
+    math::vec3 v1_screen = { float((v1_ndc[0] + 1) * 0.5 * width), float((1 - v1_ndc[1]) * 0.5 * height), v1_ndc[2] };
+    math::vec3 v2_screen = { float((v2_ndc[0] + 1) * 0.5 * width), float((1 - v2_ndc[1]) * 0.5 * height), v2_ndc[2] };
+
+	// Convert to screen coordinates
+	math::vec3 v0_screen1 = { float((v0[0] + 1) * 0.5 * width), float((1 - v0[1]) * 0.5 * height), v0[2] };
+	math::vec3 v1_screen1 = { float((v1[0] + 1) * 0.5 * width), float((1 - v1[1]) * 0.5 * height), v1[2] };
+	math::vec3 v2_screen1 = { float((v2[0] + 1) * 0.5 * width), float((1 - v2[1]) * 0.5 * height), v2[2] };
+
 
     Triangle triangle(v0_screen, v1_screen, v2_screen);
+	Triangle triangle1(v0_screen1, v1_screen1, v2_screen1);
+	triangle1.SetColors(colorT, colorT, colorT);
 
+	triangle1.drawTriangle(colorBuffer);
 	triangle.drawTriangle(colorBuffer);
 
 	colorBuffer.generateBMP("test.bmp");
--- Graphics Algorithms/Triangle.h
+++ Graphics Algorithms/Triangle.h
@@ -4,7 +4,7 @@ class Triangle
 {
 
 public:
-    Triangle(const math::vec2& v0, const math::vec2& v1, const math::vec2& v2)
+    Triangle(const math::vec3& v0, const math::vec3& v1, const math::vec3& v2)
     {
         /*auto isClockwise = [](const math::vec2 a, const math::vec2 b, const math::vec2 c) {
             return (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0]) > 0;
@@ -44,6 +44,30 @@ public:
 		return (v1[0] - v2[0]) * (p[1] - v1[1]) - (v1[1] - v2[1]) * (p[0] - v1[0]) < 0;
 	}
 
+	bool isInsideTopLeft(const math::vec2& p) const
+	{
+		float w0 = (v1_[0] - v0_[0]) * (p[1] - v0_[1]) - (v1_[1] - v0_[1]) * (p[0] - v0_[0]);
+		float w1 = (v2_[0] - v1_[0]) * (p[1] - v1_[1]) - (v2_[1] - v1_[1]) * (p[0] - v1_[0]);
+		float w2 = (v0_[0] - v2_[0]) * (p[1] - v2_[1]) - (v0_[1] - v2_[1]) * (p[0] - v2_[0]);
+
+		// top-left edges detection
+		int dx01 = (int)(v1_[0] - v0_[0]);
+		int dy01 = (int)(v1_[1] - v0_[1]);
+		int dx12 = (int)(v2_[0] - v1_[0]);
+		int dy12 = (int)(v2_[1] - v1_[1]);
+		int dx20 = (int)(v0_[0] - v2_[0]);
+		int dy20 = (int)(v0_[1] - v2_[1]);
+
+		bool t01 = (dy01 < 0 || (dy01 == 0 && dx01 > 0));
+		bool t12 = (dy12 < 0 || (dy12 == 0 && dx12 > 0));
+		bool t20 = (dy20 < 0 || (dy20 == 0 && dx20 > 0));
+
+		return
+			(w0 > 0 || (w0 == 0 && t01)) &&
+			(w1 > 0 || (w1 == 0 && t12)) &&
+			(w2 > 0 || (w2 == 0 && t20));
+	}
+
     void drawTriangle(buffer::ColorBuffer& buffer) 
     {
 		int minx = std::min({ v0_[0], v1_[0], v2_[0] });
@@ -56,35 +80,56 @@ public:
 		miny = std::max(miny, 0);
 		maxy = std::min(maxy, buffer.GetHeight() - 1);
 
+		int dx12 = (int)(v1_[0] - v0_[0]);
+		int dy12 = (int)(v1_[1] - v0_[1]);
+		int dx23 = (int)(v2_[0] - v1_[0]);
+		int dy23 = (int)(v2_[1] - v1_[1]);
+		int dx31 = (int)(v0_[0] - v2_[0]);
+		int dy31 = (int)(v0_[1] - v2_[1]);
+
+		//Top-Left
+		bool t01 = (dy12 < 0 || (dy12 == 0 && dx12 > 0));
+		bool t12 = (dy23 < 0 || (dy23 == 0 && dx23 > 0));
+		bool t20 = (dy31 < 0 || (dy31 == 0 && dx31 > 0));
+
+
 
 		for (int i = minx; i <= maxx; i++) {
 
 			for (int j = miny; j <= maxy; j++)
 			{
-				math::vec2 p = { i, j };
-				if (isLeft(v0_, v1_, p) && isLeft(v1_, v2_, p) && isLeft(v2_, v0_, p))
+				math::vec2 p = { i + 0.5f, j + 0.5f};
+				if (isInsideTopLeft(p))
 				{
 					float denom = (v1_[1] - v2_[1]) * (v0_[0] - v2_[0]) + (v2_[0] - v1_[0]) * (v0_[1] - v2_[1]);
 					float lambda1 = ((v1_[1] - v2_[1]) * (p[0] - v2_[0]) + (v2_[0] - v1_[0]) * (p[1] - v2_[1])) / denom;
 					float lambda2 = ((v2_[1] - v0_[1]) * (p[0] - v2_[0]) + (v0_[0] - v2_[0]) * (p[1] - v2_[1])) / denom;
 					float lambda3 = 1.0f - lambda1 - lambda2;
 
-					Color4 color;
-					color[0] = static_cast<unsigned char>(lambda1 * color0_[0] + lambda2 * color1_[0] + lambda3 * color2_[0]);
-					color[1] = static_cast<unsigned char>(lambda1 * color0_[1] + lambda2 * color1_[1] + lambda3 * color2_[1]);
-					color[2] = static_cast<unsigned char>(lambda1 * color0_[2] + lambda2 * color1_[2] + lambda3 * color2_[2]);
-					color[3] = static_cast<unsigned char>(lambda1 * color0_[3] + lambda2 * color1_[3] + lambda3 * color2_[3]);
+					float z = lambda1 * v0_[2] + lambda2 * v1_[2] + lambda3 * v2_[2];
 
-					buffer.SetColor(i, j, color);
+					if (z > buffer.GetColorDepth(i, j)) {
+						buffer.SetColorDepth(i, j, z);
+
+						Color4 color;
+						color[0] = static_cast<unsigned char>(lambda1 * color0_[0] + lambda2 * color1_[0] + lambda3 * color2_[0]);
+						color[1] = static_cast<unsigned char>(lambda1 * color0_[1] + lambda2 * color1_[1] + lambda3 * color2_[1]);
+						color[2] = static_cast<unsigned char>(lambda1 * color0_[2] + lambda2 * color1_[2] + lambda3 * color2_[2]);
+						color[3] = static_cast<unsigned char>(lambda1 * color0_[3] + lambda2 * color1_[3] + lambda3 * color2_[3]);
+
+						buffer.SetColor(i, j, color);
+					}
 				}
 			}
 		}
 	}
 
 private:
-	math::vec2 v0_;
-	math::vec2 v1_;
-	math::vec2 v2_;
+	math::vec3 v0_;
+	math::vec3 v1_;
+	math::vec3 v2_;
+
+
 
 	Color4 color0_;
 	Color4 color1_;
